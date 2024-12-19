c6997a9dc1ec693e91dcb38897532b0fd3d84c50
Ramez Asaad <118040774+raamezzz@users.noreply.github.com>
2024-12-19 04:54:19 +0200

    finalized powerUp.java and powerUpsCollisionThread.java

---
+++ src/main/java/com/example/demo/powerUp.java
@@ -0,0 +1,144 @@
+package com.example.demo;
+
+import javafx.application.Platform;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class powerUp implements Runnable {
+
+
+    private ImageView powerUpNode;
+    private boolean running = true;
+    private final Pane gamePane;
+    private HelloApplication.Lane lane;
+    private int positionY;
+    private int positionX;
+    int time;
+    Timer timer;
+    public double getX(){ return powerUpNode.getX(); }
+    public double getY(){ return powerUpNode.getY(); }
+    public Pane getGamePane(){ return gamePane; }
+    public void stopRunning(){ running = false; }
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setLane(HelloApplication.Lane lane) {
+        setPositionX(lane.getValue() + 30);
+    }
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+    public void setNode(String name) {
+        this.powerUpNode = new ImageView(name);
+    }
+
+    public HelloApplication.Lane getLane() {
+        return lane;
+    }
+
+    public powerUp(int time, ImageView powerUpNode, Pane gamePane) {
+        final HelloApplication.Lane[] newlane = {rocket.getRandomLane()};
+        for (rocket rocket : HelloApplication.rockets) {
+            if(rocket.getX() == newlane[0].getValue()) newlane[0] = rocket.getRandomLane();
+            else break;
+        }
+        lane = newlane[0];
+        this.powerUpNode = powerUpNode;
+        this.gamePane = gamePane;
+        this.positionY = -45;
+        this.time = time;
+        powerUpNode.setFitWidth(50);
+        powerUpNode.setFitHeight(50);
+        powerUpNode.setX(lane.getValue()+35);
+        powerUpNode.setY(positionY);
+    }
+
+    public ImageView getPowerUpNode() {
+        return powerUpNode;
+    }
+
+    public void movePowerUP(){
+        Platform.runLater(()->{
+            positionY += 2;
+            powerUpNode.setY(positionY);
+        });
+    }
+    public boolean checkCollision(rocketShip k){
+        double range = 50.0; // The range within which a collision is detected
+
+        // Check if the X-coordinate of `k` is within the range of `this`
+        boolean xCollision = Math.abs(k.getX() - this.getX()) <= range;
+
+        // Check if the Y-coordinate of `k` is within the range of `this`
+        boolean yCollision = Math.abs((k.getY()) - (this.getY()-33)) <= range;
+
+        // Return true if both X and Y are within the range
+        return xCollision && yCollision;
+    }
+    public boolean checkOutOfBound(){
+        double y = this.getY();
+        if(y>gamePane.getHeight() || y< -50){
+            return true;
+        }
+        else return false;
+    }
+    @Override
+    public void run() {
+        Platform.runLater(() -> gamePane.getChildren().add(powerUpNode));
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> gamePane.getChildren().remove(powerUpNode));
+                running = false;
+                timer.cancel();
+
+            }
+        }, time);
+
+        while(running){
+            try{
+                Thread.sleep(16);
+                movePowerUP();
+            }catch(InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
+    public void powerUpChange(){
+
+        Platform.runLater(()->{
+            setLane(rocket.getRandomLane());
+            setPositionY(-30);
+            gamePane.getChildren().remove(this.powerUpNode);
+            if(powerUpNode.getImage().getUrl().contains("star")) {
+                setNode("x-powerup.png");
+                powerUpNode.setX(getPositionX());
+                powerUpNode.setFitWidth(50);
+                powerUpNode.setFitHeight(50);
+            }
+            else {
+                setNode("star.png");
+                powerUpNode.setX(getPositionX());
+                powerUpNode.setFitWidth(50);
+                powerUpNode.setFitHeight(50);
+            }
+            gamePane.getChildren().add(this.powerUpNode);
+        });
+
+    }
+}
---
+++ src/main/java/com/example/demo/powerUpsCollisionThread.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import javafx.application.Platform;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+
+public class powerUpsCollisionThread implements Runnable {
+    Pane gamePane;
+    rocketShip rocketShip;
+    ArrayList<powerUp> powerUps;
+    String star = "star.png";
+    String xPowerUp = "x-powerup.png";
+    private volatile boolean running = true;
+    powerUp starPower;
+    powerUp xPowerUpPower;
+    powerUp currentPowerUp;
+    public powerUpsCollisionThread(rocketShip rocketShip, Pane gamePane) {
+        this.rocketShip = rocketShip;
+        this.gamePane = gamePane;
+        xPowerUpPower = new powerUp(20000,new ImageView(this.xPowerUp), gamePane );
+        starPower = new powerUp(200000000,new ImageView(this.star),gamePane);
+        currentPowerUp = starPower;
+    }
+
+    @Override
+    public void run() {
+        new Thread(currentPowerUp).start();
+        while (running) {
+                try {
+                    Thread.sleep(16);
+                    boolean collision = currentPowerUp.checkCollision(rocketShip);
+                    if (collision) {
+                        Platform.runLater(() -> {
+                            scoreRectangle.incrementScore(100);
+                            currentPowerUp.powerUpChange();
+                        });
+                    }else if(currentPowerUp.checkOutOfBound()) {
+                        Platform.runLater(()->{
+                            currentPowerUp.powerUpChange();
+                        });
+                    }
+
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+        }
+
+    }
+}
